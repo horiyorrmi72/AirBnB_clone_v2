6f9d469f69fb51c00a4c9e0f108bbea42b108835
yusuf ridwan <horiyorrmi72@gmail.com>
2022-01-31 16:24:37 -0800

    fix pep8

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,4 +1,4 @@
-#!.usr/bin/python3
+#!usr/bin/python3
 """
 starts a Flask web application
 """
@@ -7,9 +7,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def index():
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
